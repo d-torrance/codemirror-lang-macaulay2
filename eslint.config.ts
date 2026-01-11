f4f08915d2b9e134f9ea7ca3750802f3799014a9
Doug Torrance <dtorrance@piedmont.edu>
2026-01-10 23:47:22 -0500

    Add eslint support

---
+++ eslint.config.ts
@@ -0,0 +1,38 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import json from "@eslint/json";
+import markdown from "@eslint/markdown";
+import { defineConfig, globalIgnores } from "eslint/config";
+import mochaPlugin from "eslint-plugin-mocha";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  tseslint.configs.recommended,
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+  },
+  {
+    files: ["**/*.md"],
+    plugins: { markdown },
+    language: "markdown/gfm",
+    extends: ["markdown/recommended"],
+  },
+  {
+    files: ["test/*.js"],
+    plugins: { mochaPlugin },
+    extends: [mochaPlugin.configs.recommended],
+    rules: {
+      "mocha/no-setup-in-describe": "off",
+    },
+  },
+  globalIgnores(["dist", "demo/bundle.js", "package-lock.json"]),
+]);
