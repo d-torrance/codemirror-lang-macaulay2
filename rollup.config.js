b9a712ab98e902349355fe8ce8d69ba8ac0c6ad7
Doug Torrance <dtorrance@piedmont.edu>
2026-05-11 00:12:55 -0400

    Restore @rollup/plugin-typescrip to generate declarations file
    Accidentally removed it a couple releases ago!

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+import typescript from "@rollup/plugin-typescript";
+
 export default {
   input: "src/index.ts",
   external: (id) => id != "tslib" && !/^(\.?\/|\w:)/.test(id),
@@ -5,4 +7,5 @@ export default {
     { file: "dist/index.cjs", format: "cjs" },
     { dir: "./dist", format: "es" },
   ],
+  plugins: [typescript()],
 };
