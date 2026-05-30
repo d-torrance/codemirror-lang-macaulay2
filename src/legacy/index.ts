11d68cee9479259f256b781d9a803324fe904ecb
Doug Torrance <dtorrance9@gatech.edu>
2026-05-29 20:11:22 -0400

    Go ahead and register legacy mode when importing

--- src/legacy/index.ts
+++ src/legacy/index.ts
@@ -1,1 +1,9 @@
-export { Macaulay2Mode as macaulay2Mode } from "../grammar.js";
+import { Macaulay2Mode } from "../grammar.js";
+
+declare const CodeMirror: {
+  defineSimpleMode(name: string, states: object): void;
+};
+
+CodeMirror.defineSimpleMode("macaulay2", Macaulay2Mode);
+
+export { Macaulay2Mode as macaulay2Mode };
